1ee87a15e195c51d3a5d8f708114ff473ab05500
Wer108 <madhudoss750@gmail.com>
2026-09-16 20:51:35 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -1372,7 +1372,6 @@ async function setupViteOrStatic() {
   } else {
     const distPath = path.join(process.cwd(), "dist");
     app.use(express.static(distPath));
- 
     app.get("*", (_req, res) => {
       res.sendFile(path.join(distPath, "index.html"));
     });
